a314a6b05617efea8f3f9340e66711baea771eda
Thomas Mathew, Nikky (US - Mumbai) <nikky17may@gmail.com>
2020-09-07 12:52:49 +0530

    Updates

---
+++ precache-manifest.220696492cf96b584b0ebf7d5914150b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789b18fc300272f16b44a708639a0bfa",
+    "url": "/knight-tracker/index.html"
+  },
+  {
+    "revision": "02ca79fe2ae2930d8c47",
+    "url": "/knight-tracker/static/css/main.9016ac81.chunk.css"
+  },
+  {
+    "revision": "fc44a2862d8e008bf183",
+    "url": "/knight-tracker/static/js/2.91974883.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/knight-tracker/static/js/2.91974883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ca79fe2ae2930d8c47",
+    "url": "/knight-tracker/static/js/main.bce7692a.chunk.js"
+  },
+  {
+    "revision": "315da417551cba05d12c",
+    "url": "/knight-tracker/static/js/runtime-main.8a16053e.js"
+  }
+]);
